2ff17edac82338df0cd05e61630b62fec2700127
Staalduine <simonvanstaalduine@home.nl>
2025-12-15 23:04:52 +0100

    started adding geojson features

--- src/graphs/edges.rs
+++ src/graphs/edges.rs
@@ -1,4 +1,6 @@
 use bevy::math::Vec3;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 #[derive(Debug, Clone, Copy)]
 pub(crate) struct EdgeData {
@@ -9,6 +11,20 @@ impl EdgeData {
     fn length(&self) -> f32 {
         (self.node_poss.1 - self.node_poss.0).length()
     }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EdgeProperties {
+    pub diameter: Option<f64>,
+    #[serde(flatten)]
+    pub other: HashMap<String, serde_json::Value>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Edge {
+    pub source: String,
+    pub target: String,
+    pub properties: HashMap<i64, EdgeProperties>,
 }
 
 #[test]
