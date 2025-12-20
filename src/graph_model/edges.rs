79693cdd5ba85e62f71488ded7952c669a416ba2
Staalduine <simonvanstaalduine@home.nl>
2025-12-20 22:04:01 +0100

    changed edge full to accomodate sql

--- src/graph_model/edges.rs
+++ src/graph_model/edges.rs
@@ -1,8 +1,9 @@
 use bevy::{ecs::component::Component, math::Vec3};
+use serde::{Deserialize, Serialize};
 
 use crate::graph_model::types::TimeSeries;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
 pub enum EdgeProgress {
     NotStarted,
     Growing(f32), // 0.0 to 1.0
@@ -28,27 +29,20 @@ impl EdgeLight {
     }
 }
 
-#[derive(Clone, Component)]
+#[derive(Clone, Component, Debug)]
 pub struct EdgeFull {
-    pub node_poss: (Vec3, Vec3),
-    pub source: usize,
-    pub target: usize,
-    pub id: usize,
-    pub edge_cluster_id: usize,
+    pub source: i64,
+    pub target: i64,
+    pub id: i64,
+    pub edge_cluster_id: i64,
     pub temporal_props: TimeSeries<EdgeTemporals>,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct EdgeTemporals {
     pub width: f32,
     pub progress: EdgeProgress,
     pub other_data: serde_json::Map<String, serde_json::Value>,
-}
-
-impl EdgeFull {
-    pub fn length(&self) -> f32 {
-        self.node_poss.0.distance(self.node_poss.1)
-    }
 }
 
 #[test]
