f27bd5b7afc5833e4eb0e77e516aa38c52e1298a
Staalduine <vstaalduine@amolf.nl>
2025-12-22 15:41:32 +0100

    removed some unwraps added test, which passes

--- src/bevy_utils/db_setup.rs
+++ src/bevy_utils/db_setup.rs
@@ -1,26 +1,27 @@
+use std::io::Error;
 
 use crate::{
-    bevy_utils::db_usage::{DbRequestEvent, DbResponseEvent},
+    bevy_utils::db_usage::{DbRequestEvent, DbResponseEvent, DbWorker},
     graph_model::{
         edges::{EdgeFull, EdgeTemporals},
         types::TimeSeries,
     },
 };
 use crossbeam_channel::Sender;
+use crossbeam_channel::{Receiver, unbounded};
 use rusqlite::{Connection, params_from_iter};
 
+/// Generally used service linked to the database.
 pub(crate) struct DbService {
     conn: Connection,
 }
 
-use crossbeam_channel::Receiver;
-
-enum RequestHandleError {
-    Error,
+enum RequestHandleResult {
+    Success,
     Shutdown,
 }
 
-/// Opens (or creates) a SQLite database at the default application data path.
+/// Opens (or creates) a filepath at the default application data path.
 fn open_default_path_db() -> String {
     let data_dir = dirs_next::data_dir().unwrap().join("grAMF");
     std::fs::create_dir_all(&data_dir).unwrap();
@@ -29,45 +30,63 @@ fn open_default_path_db() -> String {
     data_path.to_string()
 }
 
+/// Creation of the database, should only be called at the start of the app.
+/// Continually runs during the app, consuming request events.
 pub(super) fn run_db_service(rx_req: Receiver<DbRequestEvent>, tx_res: Sender<DbResponseEvent>) {
-    let mut service = DbService::new(&open_default_path_db());
-    service.init_schema();
+    let mut service = DbService::new(&open_default_path_db()).unwrap();
+
+    let schema_result = service.init_schema();
+    match schema_result {
+        Ok(_) => (),
+        Err(schema_error) => println!("{:?}", schema_error.to_string()),
+    }
 
     while let Ok(request) = rx_req.recv() {
         match handle_request(request, &mut service, &tx_res) {
-            Ok(_) => continue,
+            Ok(result_val) => match result_val {
+                RequestHandleResult::Success => continue,
+                RequestHandleResult::Shutdown => break,
+            },
             Err(_) => break,
         }
     }
 }
 
-fn handle_request(request: DbRequestEvent, service: &mut DbService, tx_res: &Sender<DbResponseEvent>) -> Result<(), RequestHandleError> {
+/// Middle-man function to select a method based on event type.
+fn handle_request(
+    request: DbRequestEvent,
+    service: &mut DbService,
+    tx_res: &Sender<DbResponseEvent>,
+) -> Result<RequestHandleResult, Error> {
     match request {
         DbRequestEvent::InsertEdges(edges) => {
-            service.insert_edges(edges, &tx_res);
-            Ok(())
+            let result = service.insert_edges(edges).unwrap();
+            tx_res
+                .send(DbResponseEvent::InsertProgress { inserted: result })
+                .unwrap();
+            Ok(RequestHandleResult::Success)
         }
         DbRequestEvent::QueryEdges(ids) => {
-            service.query_edges(ids, &tx_res);
-            Ok(())
+            let results = service.query_edges(ids).unwrap();
+            tx_res.send(DbResponseEvent::Edges(results)).unwrap();
+            Ok(RequestHandleResult::Success)
         }
         DbRequestEvent::Shutdown => {
             let _ = tx_res.send(DbResponseEvent::ShutdownComplete);
-            Err(RequestHandleError::Shutdown)
+            Ok(RequestHandleResult::Shutdown)
         }
     }
 }
 
 impl DbService {
-    fn new(path: &str) -> Self {
-        let conn = Connection::open(path).unwrap();
-        Self { conn }
+    fn new(path: &str) -> Result<Self, rusqlite::Error> {
+        let conn = Connection::open(path)?;
+        Ok(Self { conn })
     }
 
-    fn init_schema(&self) {
-        self.conn
-            .execute_batch(
-                "
+    fn init_schema(&self) -> Result<(), rusqlite::Error> {
+        self.conn.execute_batch(
+            "
                 PRAGMA journal_mode = WAL;
                 PRAGMA synchronous = NORMAL;
 
@@ -76,50 +95,55 @@ impl DbService {
                     source          INTEGER,
                     target          INTEGER,
                     cluster_id      INTEGER,
-                    temporal_props  BLOB NOT NULL,
+                    temporal_props  BLOB NOT NULL
                 );
                 ",
-            )
-            .unwrap();
+        )
     }
 
-    fn insert_edges(&mut self, edges: Vec<EdgeFull>, tx_res: &Sender<DbResponseEvent>) {
-        let tx = self.conn.transaction().unwrap();
+    fn insert_edges(
+        &mut self,
+        edges: Vec<EdgeFull>,
+        // tx_res: &Sender<DbResponseEvent>,
+    ) -> Result<usize, rusqlite::Error> {
+        let tx = self.conn.transaction()?;
 
         {
-            let mut stmt = tx
-                .prepare(
-                    "
+            let mut stmt = tx.prepare(
+                "
                     INSERT OR REPLACE INTO edge
                     (edge_id, source, target, cluster_id, temporal_props)
                     VALUES (?1, ?2, ?3, ?4, ?5)
                     ",
-                )
-                .unwrap();
+            )?;
 
             for e in &edges {
-                // let blob = postcard::to_allocvec(&e.temporal_props)?;
                 stmt.execute((
                     e.id,
                     e.source,
                     e.target,
                     e.edge_cluster_id,
                     postcard::to_allocvec(&e.temporal_props).unwrap(),
-                ))
-                .unwrap();
+                ))?;
             }
         }
 
-        tx.commit().unwrap();
+        tx.commit()?;
 
-        let _ = tx_res.send(DbResponseEvent::InsertProgress {
-            inserted: edges.len(),
-        });
+        // let _ = tx_res.send(DbResponseEvent::InsertProgress {
+        //     inserted: edges.len(),
+        // });
+
+        Ok(edges.len())
     }
 
-    fn query_edges(&self, edge_ids: Vec<i64>, tx_res: &Sender<DbResponseEvent>) {
+    fn query_edges(
+        &self,
+        edge_ids: Vec<i64>,
+        // tx_res: &Sender<DbResponseEvent>,
+    ) -> Result<Vec<EdgeFull>, rusqlite::Error> {
         if edge_ids.is_empty() {
-            return;
+            return Err(rusqlite::Error::InvalidQuery);
         }
 
         let placeholders = edge_ids.iter().map(|_| "?").collect::<Vec<_>>().join(",");
@@ -133,28 +157,42 @@ impl DbService {
             placeholders
         );
 
-        let mut stmt = self.conn.prepare(&sql).unwrap();
+        let mut stmt = self.conn.prepare(&sql)?;
 
-        let rows = stmt
-            .query_map(params_from_iter(edge_ids.iter()), |row| {
-                let blob: Vec<u8> = row.get(4)?;
-                let temporal_props: TimeSeries<EdgeTemporals> =
-                    postcard::from_bytes(&blob).unwrap();
-                Ok(EdgeFull {
-                    id: row.get(0)?,
-                    source: row.get(1)?,
-                    target: row.get(2)?,
-                    edge_cluster_id: row.get(3)?,
-                    temporal_props,
-                })
+        let rows = stmt.query_map(params_from_iter(edge_ids.iter()), |row| {
+            let blob: Vec<u8> = row.get(4)?;
+            let temporal_props: TimeSeries<EdgeTemporals> = postcard::from_bytes(&blob).unwrap();
+            Ok(EdgeFull {
+                id: row.get(0)?,
+                source: row.get(1)?,
+                target: row.get(2)?,
+                edge_cluster_id: row.get(3)?,
+                temporal_props,
             })
-            .unwrap();
+        })?;
 
         let mut results: Vec<EdgeFull> = Vec::new();
         for r in rows {
-            results.push(r.unwrap());
+            results.push(r?);
         }
 
-        let _ = tx_res.send(DbResponseEvent::Edges(results));
+        Ok(results)
     }
+}
+
+#[test]
+fn test_database_init() {
+    let mut service = DbService::new(&open_default_path_db()).unwrap();
+
+    let _ = service.init_schema().unwrap();
+
+    let trial_data = vec![EdgeFull::new()];
+
+    let return_size = service.insert_edges(trial_data).unwrap();
+
+    assert!(return_size == 1);
+
+    let queried_edges = service.query_edges(vec![1]).unwrap();
+
+    println!("{:?}", queried_edges)
 }
