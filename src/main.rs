1c41506ae88208f3cec7fcf7d6016d7582090515
Staalduine <simonvanstaalduine@home.nl>
2026-05-12 20:07:05 +0200

    restarting

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,21 @@
-use gpui::*;
-use gpui_component::{button::*, *};
-use gpui_platform;
-// use crate::app::AppModel;
-use gpui_component_assets;
-
-use crate::app::gpui_app::AppModel;
-
-mod ap
+mod app;
 mod render;
-mod ui;
+
+use winit::event_loop::{ControlFlow, EventLoop};
 
 fn main() {
-    let app = gpui_platform::application().with_assets(gpui_component_assets::Assets);
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        pollster::block_on(run());
+    }
+}
 
-    app.run(move |app| {
-        gpui_component::init(app);
-        ui::init(app);
+async fn run() {
+    let event_loop = EventLoop::new().unwrap();
 
-        let bounds = Bounds::centered(None, size(px(800.), px(600.)), app);
-        app.spawn(async move |cx| {
-            cx.open_window(
-                WindowOptions {
-                    titlebar: Some(TitlebarOptions {
-                        title: Some("GrAMF".into()),
-                        appears_transparent: true,
-                        traffic_light_position: None,
-                    }),
-                    window_bounds: Some(gpui::WindowBounds::Windowed(bounds)),
-                    ..Default::default()
-                },
-                |window, cx| {
-                    let view = cx.new(|cx| ui::root_view::RootView::new(cx));
-                    cx.new(|cx| Root::new(view, window, cx))
-                },
-            )
-            .unwrap();
-            // cx.activate(true);
-        })
-        .detach();
-    })
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = app::app_model::App::new();
+
+    event_loop.run_app(&mut app).expect("Failed to run app");
+}
