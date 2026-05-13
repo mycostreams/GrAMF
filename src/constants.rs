403d9d3a3fb421cf022465d9486f54ecbb170385
Staalduine <simonvanstaalduine@home.nl>
2026-05-13 18:23:36 +0200

    dsds

--- src/constants.rs
+++ src/constants.rs
@@ -91,34 +91,3 @@ pub const GREEN_CUBE_VERTICES: [Vertex; 8] = [
         color: [0.0, 1.0, 0.0, 1.0],
     },
 ];
-
-pub const SHADER_SOURCE: &str = "
-struct Uniform {
-    mvp: mat4x4<f32>,
-};
-
-@group(0) @binding(0)
-var<uniform> ubo: Uniform;
-
-struct VertexInput {
-    @location(0) position: vec4<f32>,
-    @location(1) color: vec4<f32>,
-};
-struct VertexOutput {
-    @builtin(position) position: vec4<f32>,
-    @location(0) color: vec4<f32>,
-};
-
-@vertex
-fn vertex_main(vert: VertexInput) -> VertexOutput {
-    var out: VertexOutput;
-    out.color = vert.color;
-    out.position = ubo.mvp * vert.position;
-    return out;
-};
-
-@fragment
-fn fragment_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    return vec4<f32>(in.color);
-}
-";
