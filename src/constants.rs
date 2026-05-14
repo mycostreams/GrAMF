4e49c4b61ad283167b41a784c8a4fc062d04cc3b
Staalduine <simonvanstaalduine@home.nl>
2026-05-14 11:05:51 +0200

    made edge into square

--- src/constants.rs
+++ src/constants.rs
@@ -1,21 +1,25 @@
 use crate::vertex::Vertex;
 
-pub const VERTICES: [Vertex; 3] = [
+pub const EDGE_VERTS: [Vertex; 4] = [
     Vertex {
-        position: [1.0, -1.0, 0.0, 1.0],
+        position: [-0.5, -0.5, 0.0, 1.0],
         color: [1.0, 0.0, 0.0, 1.0],
     },
     Vertex {
-        position: [-1.0, -1.0, 0.0, 1.0],
+        position: [0.5, -0.5, 0.0, 1.0],
         color: [0.0, 1.0, 0.0, 1.0],
     },
     Vertex {
-        position: [0.0, 1.0, 0.0, 1.0],
+        position: [-0.5, 0.5, 0.0, 1.0],
         color: [0.0, 0.0, 1.0, 1.0],
+    },
+    Vertex {
+        position: [0.5, 0.5, 0.0, 1.0],
+        color: [1.0, 1.0, 0.0, 1.0],
     },
 ];
 
-pub const INDICES: [u32; 3] = [0, 1, 2];
+pub const EDGE_INDICES: [u32; 4] = [0, 1, 2, 3];
 
 pub const CUBE_VERTICES: [Vertex; 8] = [
     Vertex {
