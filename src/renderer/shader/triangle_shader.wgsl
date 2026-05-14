4c584184302d6ce6da759997d475ddaa91074cd6
Staalduine <simonvanstaalduine@home.nl>
2026-05-14 20:36:44 +0200

    checkpoint

--- src/renderer/shader/triangle_shader.wgsl
+++ src/renderer/shader/triangle_shader.wgsl
@@ -1,19 +1,25 @@
+//Projection matrix
 struct Uniform {
     mvp: mat4x4<f32>,
 };
 
+// Bind the uniform buffer to group 0, binding 0
 @group(0) @binding(0)
 var<uniform> ubo: Uniform;
 
+// Vertex input, defined by position and color
 struct VertexInput {
     @location(0) position: vec4<f32>,
     @location(1) color: vec4<f32>,
 };
+
+//Vertex output, defined by position and color
 struct VertexOutput {
     @builtin(position) position: vec4<f32>,
     @location(0) color: vec4<f32>,
 };
 
+// Vertex shader: Transforms vertex positions using the MVP matrix
 @vertex
 fn vertex_main(vert: VertexInput) -> VertexOutput {
     var out: VertexOutput;
@@ -22,6 +28,7 @@ fn vertex_main(vert: VertexInput) -> VertexOutput {
     return out;
 };
 
+// Fragment shader: Outputs the interpolated color from the vertex shader
 @fragment
 fn fragment_main(in: VertexOutput) -> @location(0) vec4<f32> {
     return vec4<f32>(in.color);
--- src/vertex/mod.rs
+++ src/vertex/mod.rs
@@ -23,6 +23,55 @@ impl Vertex {
     }
 }
 
+pub struct EdgeInstance {
+    pub start_position: cgmath::Vector3<f32>,
+    pub end_position: cgmath::Vector3<f32>,
+    pub color: cgmath::Vector4<f32>,
+    pub width: f32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct EdgeInstanceRaw {
+    pub start_position: [f32; 3],
+    pub end_position: [f32; 3],
+    pub color: [f32; 4],
+    _padding: [f32; 3], // Padding to ensure 16-byte alignment for the width field
+    pub width: f32,
+}
+
+impl EdgeInstance {
+    fn to_raw(&self) -> EdgeInstanceRaw {
+        EdgeInstanceRaw {
+            start_position: self.start_position.into(),
+            end_position: self.end_position.into(),
+            color: self.color.into(),
+            _padding: [0.0; 3],
+            width: self.width,
+        }
+    }
+}
+
+impl EdgeInstanceRaw {
+    pub fn vertex_attributes() -> Vec<VertexAttribute> {
+        vertex_attr_array![
+            2 => Float32x3, // start_position
+            3 => Float32x3, // end_position
+            4 => Float32x4, // color
+            5 => Float32,   // width
+        ]
+        .to_vec()
+    }
+
+    pub fn description(attributes: &[VertexAttribute]) -> VertexBufferLayout<'_> {
+        VertexBufferLayout {
+            array_stride: std::mem::size_of::<EdgeInstanceRaw>() as wgpu::BufferAddress,
+            step_mode: VertexStepMode::Instance,
+            attributes,
+        }
+    }
+}
+
 // #[repr(C)]
 // #[derive(Default, Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
 // pub struct UniformBuffer {
